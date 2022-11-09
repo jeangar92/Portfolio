2019e5a79d57c0bdcb047a992d22a0dbe6177224
Jean Garcia <jeangar92@gmail.com>
2022-11-09 18:29:41 -0500

    changeed the way the tab switching is written in js and styled it different

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
+function openTab(evt, tabTitle) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  document.getElementById(tabTitle).style.display = "block";
+  evt.currentTarget.className += " active";
+}
